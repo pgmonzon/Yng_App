ae363cc27dfc84f9f7bd123199f8a6e098d33923
Adrian Dias da Costa Lima <adrian.diasdacostalima@gmail.com>
2016-10-24 23:49:54 -0300

    andando

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,11 +1,21 @@
+import { Injectable }    from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
-import { Injectable } from '@angular/core';
 
 @Injectable()
 export class HeroService {
+  private heroesUrl = 'http://localhost:3113/api/heroes';  // URL to web api
+
+  constructor(private http: Http) { }
+
   getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    return this.http.get(this.heroesUrl)
+               .toPromise()
+               .then(response => <Hero[]>response.json())
+               .catch(this.handleError);
   }
 
   getHeroesSlowly(): Promise<Hero[]> {
@@ -17,5 +27,9 @@ export class HeroService {
   getHero(id: number): Promise<Hero> {
     return this.getHeroes()
                .then(heroes => heroes.find(hero => hero.id === id));
+  }
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
   }
 }
