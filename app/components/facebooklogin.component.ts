2953b010b6bff2c0054725f227f8d48f2799f1b5
Adrian Dias da Costa Lima <adrian.diasdacostalima@gmail.com>
2016-11-25 21:16:14 -0300

    bugfixes y orden

--- app/components/facebooklogin.component.ts
+++ app/components/facebooklogin.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, NgZone } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService }  from '../login.service';
+import { FacebookLogin } from '../social-login';
 import { Router } from '@angular/router';
 
 declare const FB:any;
@@ -11,51 +13,68 @@ declare const gapi:any;
 
 export class FacebookLoginComponent implements OnInit {
 
-    constructor(private _zone: NgZone) {
+    conectado = null;
+    userAuthToken = null;
+    userDisplayName = "empty";
+
+    constructor(private loginService: LoginService) {
 
         FB.init({
             appId      : '146855582455399',
             cookie     : false,  // enable cookies to allow the server to access the session
             xfbml      : true,  // parse social plugins on this page
-            version    : 'v2.5' // use graph api version 2.5
+            version    : 'v2.8' // use graph api version 2.5
         });
     }
 
-    onSignIn = (loggedInUser) => {
-      this._zone.run(() => {
-        console.log(loggedInUser.getBasicProfile().getName());
-      });
-    }
-
     onFacebookLoginClick() {
-      FB.login( (result: any) => { console.log(result) }     );
-      this.FacebookApi();
+      //Chequeamos si está conectado o no. Si no lo está, lo intentamos loguear.
+      FB.getLoginStatus(response => { (this.conectado = this.statusChangeCallback(response)); } );
+      if (!this.conectado){
+        FB.login();
+      } else {
+        this.FacebookApi();
+      }
+
     }
 
-    FacebookApi(){
-      FB.api('/me?fields=id,name,gender,birthday,email,picture.width(150).height(150),age_range,friends',
-          function(result) {
+    FacebookApi(): any {
+      FB.api('/me?fields=id,name,link,gender,birthday,email,picture.width(150).height(150),age_range,friends',
+          (result) => {
               if (result && !result.error) {
-                  this.user = result;
-                  console.log(this.user);
+                  let user = result;
+                  console.log(user);
+                  this.loguear(user);
+                  this.userDisplayName = result.name;
               } else {
                   console.log(result.error);
               }
           });
     }
 
+    get username() { return this.userDisplayName; }
+
+    loguear(user: string) {
+      console.log("LOGUEANDO:", user);
+      this.loginService.loguear(user);
+    }
+
     statusChangeCallback(resp) {
         if (resp.status === 'connected') {
+            console.log("Estamos listossssssssss <-");
+            return true;
             // connect here with your server for facebook login by passing access token given by facebook
         }else if (resp.status === 'not_authorized') {
+            console.log("No estamos autorizados");
 
         }else {
-
+            console.log("No estamos conectados");
+            return false;
         }
-    };
+    }
+
     ngOnInit() {
-        FB.getLoginStatus(response => { console.log(this.statusChangeCallback(response) ); } );
-        console.log(FB.getLoginStatus());
+        FB.getLoginStatus(response => { console.log(this.statusChangeCallback(response) + ' Esto es ngOnInit'), (console.log(response)); } );
     }
 
     ngAfterViewInit() {
@@ -65,7 +84,15 @@ export class FacebookLoginComponent implements OnInit {
           'height': 50,
           'longtitle': true,
           'theme': 'dark',
-          'onSuccess': this.onSignIn
+          'onSuccess': this.onGoogleLoginSuccess,
+          'onfailure': function(err){console.log("error: "+err);}
       });
     }
+
+    onGoogleLoginSuccess(loggedInUser) {
+      this.userAuthToken = loggedInUser.getAuthResponse().id_token;
+      this.userDisplayName = loggedInUser.getBasicProfile().getName();
+      console.log(this);
+    }
+
 }
