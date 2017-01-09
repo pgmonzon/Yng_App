85740622c9cc8a3887686799fe3c48545ea23b05
Adrian Dias da Costa Lima <adrian.diasdacostalima@gmail.com>
2017-01-09 16:18:11 -0300

    orden y navbar listoA

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,30 +7,7 @@ import { AppComponentLoginService } from './token.service';
 @Component({
   moduleId: module.id,
   selector: 'my-app',
-  template: `
-    <h1><a routerLink="/">{{title}}</a></h1>
-      <div *ngIf="logueado" ngbDropdown class="d-inline-block">
-          <button class="btn btn-outline-primary" id="dropdownMenu1" ngbDropdownToggle>{{usuario}}</button>
-          <div class="dropdown-menu" aria-labelledby="dropdownMenu1">
-              <a href="/logout" class="dropdown-item">Logout</a>
-          </div>
-      </div>
-      <div *ngIf="!logueado">
-        <nav><a routerLink="/login" routerLinkActive="active">Login</a></nav>
-      </div>
-    <!--</div> -->
-
-      <!--<nav>
-        <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
-        <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
-        <a routerLink="/login" routerLinkActive="active">Login</a>
-        <a routerLink="/register" routerLinkActive="active">Register</a>
-        <a routerLink="/codigo" routerLinkActive="active">Codigo</a>
-        <a routerLink="/olvido" routerLinkActive="active">Olvido</a>
-        <a routerLink="/fb" routerLinkActive="active">Social Login</a>
-        </nav>-->
-    <router-outlet></router-outlet>
-  `,
+  templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
 })
 export class AppComponent {
